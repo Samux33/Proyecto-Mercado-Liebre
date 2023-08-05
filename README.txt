b23a67d9c9c3f9e30f986fe046e5f1ad9fef1f68
Samux33 <samuelagustin332008@gmail.com>
2023-08-05 19:44:38 -0300

    scripts para iniciar

--- README.txt
+++ README.txt
@@ -1,3 +1,2 @@
-El servidor se inicia con npx nodemon app.js(usar antes npm install)
 Para el diseño responsive use breackpoints diferentes para que la pagina 
 quede mas atractiva a la vista a mi opinion.
--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path=require('path')
 
 app.use(express.static('public'))
 
-app.listen(4000,()=>{
-    console.log("Servidor Corriendo")
-})
+const port=process.env.port||3001;
+
+
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto `+port));
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
